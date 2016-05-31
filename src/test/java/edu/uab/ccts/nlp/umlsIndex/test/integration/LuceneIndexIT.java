6d57a2b5378624aabd269812881212efdb46bddc
John David Osborne <ozborn@uab.edu>
2016-05-31 10:57:12 -0500

    Poorly performing UMLS search using Lucene

--- src/test/java/edu/uab/ccts/nlp/umlsIndex/test/integration/LuceneIndexIT.java
+++ src/test/java/edu/uab/ccts/nlp/umlsIndex/test/integration/LuceneIndexIT.java
@@ -1,38 +1,67 @@
 package edu.uab.ccts.nlp.umlsIndex.test.integration;
 
-import junit.framework.Test;
-import junit.framework.TestCase;
-import junit.framework.TestSuite;
+import static org.junit.Assert.*;
+
+import java.io.IOException;
+import java.nio.file.Paths;
+import org.junit.Test;
+
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.index.DirectoryReader;
+import org.apache.lucene.queryparser.classic.ParseException;
+import org.apache.lucene.queryparser.classic.QueryParser;
+import org.apache.lucene.search.IndexSearcher;
+import org.apache.lucene.search.Query;
+import org.apache.lucene.search.ScoreDoc;
+import org.apache.lucene.search.TopDocs;
+import org.apache.lucene.store.FSDirectory;
+
 
 /**
  * Unit test for simple App.
  */
 public class LuceneIndexIT 
-    extends TestCase
 {
-    /**
-     * Create the test case
-     *
-     * @param testName name of the test case
-     */
-    public LuceneIndexIT( String testName )
-    {
-        super( testName );
-    }
+	private IndexSearcher searcher = null;
+	private QueryParser parser = null;
+	/**
+	 * Create the test case
+	 *
+	 * @param testName name of the test case
+	 */
+	public LuceneIndexIT() throws IOException
+	{
+		searcher = new IndexSearcher(DirectoryReader.open(FSDirectory.open(Paths.get("target/index.lucene"))));
+		parser = new QueryParser("stemmedTerms", new StandardAnalyzer());
+	}
 
-    /**
-     * @return the suite of tests being tested
-     */
-    public static Test suite()
-    {
-        return new TestSuite( LuceneIndexIT.class );
-    }
+	
+	/**
+	 */
+	@org.junit.Test
+	public void testIndex() throws Exception
+	{
+		TopDocs td = performSearch("cancer", 2);
+		ScoreDoc[] hits = td.scoreDocs;
+		System.out.println("Number of hits: " + hits.length);
+	    for (int i = 0; i < hits.length; i++) {
+	        Document hitDoc = searcher.doc(hits[i].doc);
+	        System.out.println(hitDoc.get("stemmedTerms"));
+	        System.out.println(hitDoc.get("cui"));
+	        System.out.println(hitDoc.get("sty"));
+	        //assertEquals("C0814136", hitDoc.get("cui"));
+	      }
+	}
 
-    /**
-     * Rigourous Test :-)
-     */
-    public void testApp()
-    {
-        assertTrue( true );
-    }
+	public TopDocs performSearch(String queryString, int n)
+			throws IOException, ParseException {
+		Query query = parser.parse(queryString);
+		return searcher.search(query, n);
+	}
+
+	public Document getDocument(int docId)
+			throws IOException {
+		return searcher.doc(docId);
+	}
 }
